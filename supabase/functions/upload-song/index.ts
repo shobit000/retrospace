6802054300978645a67c89d29355ba7ba0deffde
Shobit garg <122355051+shobit000@users.noreply.github.com>
2025-04-10 23:01:14 +0530

    Update index.ts

--- supabase/functions/upload-song/index.ts
+++ supabase/functions/upload-song/index.ts
@@ -5,7 +5,6 @@ const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 }
-
 serve(async (req) => {
   // Handle CORS preflight requests
   if (req.method === 'OPTIONS') {
